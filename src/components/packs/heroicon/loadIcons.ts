e22dea2eb5f6afcce16565d83be54805ef217e8f
abolraj <abolfazlrajaeenasab@gmail.com>
2026-05-15 00:44:05 +0330

    feat(compoents/packs): Handle Hericons worker & load & list

---
+++ src/components/packs/heroicon/loadIcons.ts
@@ -0,0 +1,19 @@
+// packs/lucide/loadIcons.ts
+
+import * as HeroIcons from "@heroicons/react/24/solid";
+
+
+export type IconComponent = React.ComponentType<{ size?: number; className?: string; style?: object; }>;
+
+export function loadHeroIcons() {
+    const metaArray: Array<string> = [];
+    const componentMap = new Map<string, IconComponent>();
+
+    for (const [name, component] of Object.entries(HeroIcons)) {
+        console.log(name, component)
+        metaArray.push(name);
+        componentMap.set(name, component as IconComponent);
+    }
+
+    return { metaArray, componentMap };
+}
