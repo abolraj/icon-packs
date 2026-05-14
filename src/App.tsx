c04ddc42d7ec4709aa82bafa6b60bdd684abb5d6
abolraj <abolfazlrajaeenasab@gmail.com>
2026-05-15 00:44:49 +0330

    feat(App): Handle layout and contents

--- src/App.tsx
+++ src/App.tsx
@@ -1,122 +1,81 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import { useState } from "react";
+import ColorPicker from "./components/ColorPicker";
+import PackSelect from "./components/PackSelect";
+import Search from "./components/Search";
+import { Heart } from "lucide-react";
+import { SiGithub } from "@icons-pack/react-simple-icons";
+import LucideIconsList from "./components/packs/lucide/LucideIconsList";
+import HeroIconsList from "./components/packs/heroicon/HeroIconsList";
+import SIconsList from "./components/packs/si/SIconsList";
+import ThemePicker from "./components/ThemePicker";
 
-function App() {
-  const [count, setCount] = useState(0)
+const colors = [
+  '#2196F3',
+  '#F44336',
+  '#4CAF50',
+  '#9C27B0',
+  '#00BCD4',
+  '#FFEB3B',
+  '#FEFEFE',
+  '#151515',
+];
+
+const packs = [
+  'Lucide',
+  'Heroicon',
+  'SI React',
+];
+
+export default function App() {
+  const [color, setColor] = useState<string>(colors[0]);
+  const [pack, setPack] = useState<string>(packs[0]);
+  const [query, setQuery] = useState<string>('');
+  const [dark, setDark] = useState<boolean>(true);
 
   return (
-    <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.tsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          type="button"
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
+    <div className="h-full w-full flex flex-col">
+      <header className="shrink-0 flex h-10 w-full">
+        <ColorPicker
+          colors={colors}
+          color={color}
+          onChangeColor={c => setColor(c)}
+        />
+        <Search
+          onSearch={s => setQuery(s)}
+          className="grow"
+          placeholder={`Search ${pack} icons...`}
+        />
+        <ThemePicker
+          dark={dark}
+          onChangeDark={dark => setDark(dark)}
+        />
+        <PackSelect
+          packs={packs}
+          pack={pack}
+          onChangePack={p => setPack(p)}
+        />
+      </header>
 
-      <div className="ticks"></div>
 
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
+      <main className="grow overflow-x-auto">
+        {pack === 'Lucide' && <LucideIconsList query={query} color={color} />}
+        {pack === 'Heroicon' && <HeroIconsList query={query} color={color} />}
+        {pack === 'SI React' && <SIconsList query={query} color={color} />}
+        {/* <SIconsList query={query} color={color}/> */}
+      </main>
 
-      <div className="ticks"></div>
-      <section id="spacer"></section>
-    </>
-  )
+
+      <footer className="font-[NFPixel] shrink-0 text-lg flex gap-1 items-center h-10 px-2 border-t">
+        Made with
+        <Heart className="size-4 fill-red-500 stroke-none" />
+        by
+        <a href="https://abolfazlrajaee.ir" target="blank" className="text-primary hover:underline">
+          Abolfazl
+        </a>
+        <div className="grow text-right">
+          <SiGithub className="size-6 ml-auto" />
+        </div>
+      </footer>
+    </div>
+  );
 }
-
-export default App
